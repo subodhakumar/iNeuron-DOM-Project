0525119182d1556694e3bb6c55e15c61aa23d8ee
subodhakumar <subodhamail@gmail.com>
2023-03-02 13:24:35 +0530

    Project 2 done

---
+++ DOM Assignment 2.0 1,2,3/contact/contact.js
@@ -0,0 +1,7 @@
+const userName = document.querySelector(".userName");
+const userEmail = document.querySelector(".userEmail");
+const userMessage = document.querySelector(".userMessage");
+const enterName = document.querySelector(".enterName");
+const enterMail = document.querySelector(".enterMail");
+const enterMessage = document.querySelector(".enterMessage");
+
