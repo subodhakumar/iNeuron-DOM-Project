0d0420055f7cc85d9c62649faeae86d1b685a3f6
subodhakumar <subodhamail@gmail.com>
2023-03-06 13:17:37 +0530

    Project 2 done

---
+++ 04_DOM Project/04_DOM Project/script.js
@@ -0,0 +1,20 @@
+let color = document.getElementsByClassName("clash-card__unit-stats");
+color[0].style.backgroundColor = "#bd7c2f";
+color[1].style.backgroundColor = "#ee5487";
+color[2].style.backgroundColor = "#f6901a";
+color[3].style.backgroundColor = "#82bb30";
+color[4].style.backgroundColor = "#4facff";
+
+let textColor = document.getElementsByClassName("no-border");
+textColor[0].style.color = "white";
+textColor[1].style.color = "white";
+textColor[2].style.color = "white";
+textColor[3].style.color = "white";
+textColor[4].style.color = "white";
+
+let allTextColor = document.getElementsByClassName("clearfix");
+allTextColor[0].style.color = "white";
+allTextColor[1].style.color = "white";
+allTextColor[2].style.color = "white";
+allTextColor[3].style.color = "white";
+allTextColor[4].style.color = "white";
---
+++ 05_DOM Project/05_DOM Project/script.js
@@ -0,0 +1,7 @@
+let selector = document.getElementsByClassName("nav-center");
+
+let newDiv = document.createElement("div");
+newDiv.innerHTML = `<a href="index.html" class="btn"> contact </a>`;
+
+selector.appendChild("newDiv");
+
