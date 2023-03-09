efa94d5ac83a0266d0ec18e9a887cd6e9b9c5e48
subodhakumar <subodhamail@gmail.com>
2023-03-09 10:17:17 +0530

    Project 2 done

--- 05_DOM Project/05_DOM Project/script.js
+++ 05_DOM Project/05_DOM Project/script.js
@@ -1,1 +1,5 @@
+let selector = document.querySelector(".nav-center");
 
+let NewDiv = document.createElement("div");
+NewDiv.innerHTML = `<a href="index.html" class="btn"> Pro Subscription </a>`;
+selector.appendChild(NewDiv);
